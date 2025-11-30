446f3d7ac34a7068aa97397440017dd8531dc078
Musaddique Qazi <38559064+mfmqazi@users.noreply.github.com>
2025-11-29 18:30:53 -0700

    Fix 404 error by configuring multi-page build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
     base: '/prophetic-routine/',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                shamail: resolve(__dirname, 'shamail.html'),
+            },
+        },
+    },
 })
